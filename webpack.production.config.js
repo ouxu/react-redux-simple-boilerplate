8a180638bf1d039569b454bf074ed6afb31623c2
out_xu <out_xu@outlook.com>
2017-04-17 22:49:24 +0800

    Update webpack.production.config.js

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -11,7 +11,7 @@ module.exports = {
         vendors: ['react', 'react-dom', 'react-router']  //第三方库和框架另外打包
     },
     output: {
-        path: './dist/build/',
+        path: '/dist/build/',
         // publicPath: '', //有需要请自己配置，表示 index.html 中引入资源的前缀path
         filename: 'js/bundle.[chunkhash:8].js',
         chunkFilename: 'js/[name].[chunkhash:8].js'
